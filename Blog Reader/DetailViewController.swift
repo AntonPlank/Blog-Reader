8e25fd69f49f31402765664d4ea65818365cfb83
Anton Plank <anton.plank@me.com>
2016-02-29 15:37:53 +0100

    MasterViewController: shows recent google blog posts, serialization of JSON file provided by google, generates table view DetailedViewController: shows selected blog post in webView of separate View Controller

--- Blog Reader/DetailViewController.swift
+++ Blog Reader/DetailViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
 
+    @IBOutlet var webView: UIWebView!
 
     var detailItem: AnyObject? {
         didSet {
@@ -19,27 +19,29 @@ class DetailViewController: UIViewController {
             self.configureView()
         }
     }
-
+    
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.valueForKey("timeStamp")!.description
+            if let postWebview = self.webView {
+                
+                postWebview.loadHTMLString(detail.valueForKey("content")!.description, baseURL: NSURL(string:"http://googleblog.blogspot.co.uk/"));
+                
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
